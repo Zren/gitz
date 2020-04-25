0150f99269a12998adedb4892ffe2f2547680c04
Chris Holland <zrenfire@gmail.com>
2020-04-25 00:23:37 -0400

    Move left/right view code into seperate classes

--- gitz.py
+++ gitz.py
@@ -17,49 +17,28 @@ cwdAbs = os.path.abspath(os.path.expanduser(cwd))
 LOG_PATTERN = r'^([ \*\|\\\/]+)((\w{7}) (\([^)]+\) )?(.+))?$'
 print(LOG_PATTERN)
 
+class MonospaceView(Gtk.TextView):
+	def __init__(self):
+		Gtk.TextView.__init__(self)
+		self.set_monospace(True)
+		self.set_editable(False)
 
-class MainWindow(Gtk.ApplicationWindow):
+	def getAllText(self):
+		buf = self.get_buffer()
+		return buf.get_text(buf.get_start_iter(), buf.get_end_iter(), True)
 
-	def __init__(self, app):
-		Gtk.Window.__init__(self, title="TextView Example", application=app)
-		self.set_default_size(1280, 720)
+	def getLineAt(self, offset):
+		buf = self.get_buffer()
+		startIter = buf.get_iter_at_offset(offset)
+		startIter = buf.get_iter_at_line(startIter.get_line())
+		endIter = startIter.copy()
+		endIter.forward_to_line_end()
+		line = buf.get_text(startIter, endIter, True)
+		return line
 
-		# Force dark theme
-		settings = Gtk.Settings.get_default()
-		settings.set_property("gtk-theme-name", "Adwaita")
-		settings.set_property("gtk-application-prefer-dark-theme", True)
-
-		#--- Left
-		self.leftTextView = Gtk.TextView()
-		self.leftTextView.set_monospace(True)
-		self.leftTextView.set_editable(False)
-		self.leftTextBuffer = self.leftTextView.get_buffer()
-		self.leftTextBuffer.connect('notify::cursor-position', self.on_left_move_cursor)
-		self.populateLeftView()
-
-		self.leftPane = Gtk.ScrolledWindow()
-		self.leftPane.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-		self.leftPane.add(self.leftTextView)
-
-		#--- Right
-		self.selectedSha = ''
-
-		self.rightTextView = Gtk.TextView()
-		self.leftTextView.set_monospace(True)
-		self.leftTextView.set_editable(False)
-		# self.rightTextView.set_wrap_mode(Gtk.WrapMode.WORD)
-		self.rightTextBuffer = self.rightTextView.get_buffer()
-
-		self.rightPane = Gtk.ScrolledWindow()
-		self.rightPane.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-		self.rightPane.add(self.rightTextView)
-
-		#---
-		self.pane = Gtk.HPaned()
-		self.pane.add1(self.leftPane)
-		self.pane.add2(self.rightPane)
-		self.pane.set_position(600)
-		self.add(self.pane)
+class HistoryView(MonospaceView):
+	def __init__(self):
+		MonospaceView.__init__(self)
 
 	def populateLeftView(self):
 		cmd = [
@@ -75,26 +54,27 @@ class MainWindow(Gtk.ApplicationWindow):
 		]
 		process = subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True)
 		logStdout = process.stdout
-		self.leftTextBuffer.set_text(logStdout)
+		self.get_buffer().set_text(logStdout)
 		self.formatLeftView()
 
 	def formatLeftView(self):
-		self.tag_bold = self.leftTextBuffer.create_tag("bold", weight=Pango.Weight.BOLD)
-		self.tag_graph = self.leftTextBuffer.create_tag("graph", foreground="#1abc9c") # Normal
-		self.tag_sha = self.leftTextBuffer.create_tag("sha", foreground="#dfaf8f") # Orange / Color4
-		self.tag_head = self.leftTextBuffer.create_tag("head", foreground="#8cd0d3") # Cyan / Color7
-		self.tag_remote = self.leftTextBuffer.create_tag("remote", foreground="#dca3a3") # Red / Color2
-		self.tag_local = self.leftTextBuffer.create_tag("local", foreground="#72d5a3") # Green / Color3
-		self.tag_tag = self.leftTextBuffer.create_tag("tag", foreground="#f0dfaf") # Yellow / Color4
-		self.tag_summary = self.leftTextBuffer.create_tag("summary", foreground="#1abc9c") # Normal
+		buf = self.get_buffer()
+		self.tag_bold = buf.create_tag("bold", weight=Pango.Weight.BOLD)
+		self.tag_graph = buf.create_tag("graph", foreground="#1abc9c") # Normal
+		self.tag_sha = buf.create_tag("sha", foreground="#dfaf8f") # Orange / Color4
+		self.tag_head = buf.create_tag("head", foreground="#8cd0d3") # Cyan / Color7
+		self.tag_remote = buf.create_tag("remote", foreground="#dca3a3") # Red / Color2
+		self.tag_local = buf.create_tag("local", foreground="#72d5a3") # Green / Color3
+		self.tag_tag = buf.create_tag("tag", foreground="#f0dfaf") # Yellow / Color4
+		self.tag_summary = buf.create_tag("summary", foreground="#1abc9c") # Normal
 
 		def applyTagForGroup(match, group, tag):
 			start = match.start(group)
 			end = match.end(group)
 			print(match, start, end)
-			startIter = self.leftTextBuffer.get_iter_at_offset(start)
-			endIter = self.leftTextBuffer.get_iter_at_offset(end)
-			self.leftTextBuffer.apply_tag(tag, startIter, endIter)
+			startIter = buf.get_iter_at_offset(start)
+			endIter = buf.get_iter_at_offset(end)
+			buf.apply_tag(tag, startIter, endIter)
 
 		for match in re.finditer(LOG_PATTERN, self.getAllText(), re.MULTILINE):
 			applyTagForGroup(match, 1, self.tag_graph)
@@ -102,27 +82,12 @@ class MainWindow(Gtk.ApplicationWindow):
 			applyTagForGroup(match, 4, self.tag_head)
 			applyTagForGroup(match, 5, self.tag_summary)
 
-	def getAllText(self):
-		return self.leftTextBuffer.get_text(self.leftTextBuffer.get_start_iter(), self.leftTextBuffer.get_end_iter(), True)
-
-	def getLineAt(self, offset):
-		startIter = self.leftTextBuffer.get_iter_at_offset(offset)
-		startIter = self.leftTextBuffer.get_iter_at_line(startIter.get_line())
-		endIter = startIter.copy()
-		endIter.forward_to_line_end()
-		line = self.leftTextBuffer.get_text(startIter, endIter, True)
-		return line
 
 
-	def on_left_move_cursor(self, buffer, data=None):
-		line = self.getLineAt(self.leftTextBuffer.props.cursor_position)
-		print(line)
-		match = re.match(LOG_PATTERN, line)
-		if match:
-			sha = match.group(3)
-			if sha:
-				print(sha)
-				self.selectSha(sha)
+class CommitView(MonospaceView):
+	def __init__(self):
+		MonospaceView.__init__(self)
+		# self.set_wrap_mode(Gtk.WrapMode.WORD)
 
 	def selectSha(self, sha):
 		if sha == self.selectSha:
@@ -140,7 +105,58 @@ class MainWindow(Gtk.ApplicationWindow):
 		self.populateRightView(commitStdout)
 
 	def populateRightView(self, commitStdout):
-		self.rightTextBuffer.set_text(commitStdout)
+		self.get_buffer().set_text(commitStdout)
+
+
+
+class MainWindow(Gtk.ApplicationWindow):
+
+	def __init__(self, app):
+		Gtk.Window.__init__(self, title="TextView Example", application=app)
+		self.set_default_size(1280, 720)
+
+		# Force dark theme
+		settings = Gtk.Settings.get_default()
+		settings.set_property("gtk-theme-name", "Adwaita")
+		settings.set_property("gtk-application-prefer-dark-theme", True)
+
+		#--- Left
+		self.leftTextView = HistoryView()
+		leftTextBuffer = self.leftTextView.get_buffer()
+		leftTextBuffer.connect('notify::cursor-position', self.on_left_move_cursor)
+		self.leftTextView.populateLeftView()
+
+		self.leftPane = Gtk.ScrolledWindow()
+		self.leftPane.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+		self.leftPane.add(self.leftTextView)
+
+		#--- Right
+		self.selectedSha = ''
+
+		self.rightTextView = CommitView()
+		self.rightTextBuffer = self.rightTextView.get_buffer()
+
+		self.rightPane = Gtk.ScrolledWindow()
+		self.rightPane.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+		self.rightPane.add(self.rightTextView)
+
+		#---
+		self.pane = Gtk.HPaned()
+		self.pane.add1(self.leftPane)
+		self.pane.add2(self.rightPane)
+		self.pane.set_position(600)
+		self.add(self.pane)
+
+
+	def on_left_move_cursor(self, buffer, data=None):
+		line = self.leftTextView.getLineAt(buffer.props.cursor_position)
+		print(line)
+		match = re.match(LOG_PATTERN, line)
+		if match:
+			sha = match.group(3)
+			if sha:
+				print(sha)
+				self.rightTextView.selectSha(sha)
 
 
 
